fa5e32fa7d39af855fd57d78aa81a554cd88bd9d
Shivank Kapur <115289871+ShivankK26@users.noreply.github.com>
2025-07-02 20:55:31 +0530

    Delete src directory

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-
-pub mod trading_engine;
-pub mod models;
-
-// Re-export key items for easier use
-pub use models::{Order, Trade, Orderbook, ProcessingResult, OrderSide, OrderType};
-pub use trading_engine::process_orders;
